aa92aacb661a624d0dabe940937882436bcd5ec5
pradipsathe <pradipsathe33@gmail.com>
2026-03-03 13:02:40 +0530

    chore(setup) : initialize project with django, PostgreSQL and Docker configuration

---
+++ backend/config/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+import os
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
