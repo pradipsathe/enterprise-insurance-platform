40dc21af8fd01b13cc2bd4c8b9e7f59c48a92f9f
pradipsathe <pradipsathe33@gmail.com>
2026-03-28 13:25:50 +0530

    feat(policy): implement Policy APIs with filtering and search

--- backend/apps/policies/views.py
+++ backend/apps/policies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+from .models import Policy
+from .serializers import PolicySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+class PolicyListAPIView(generics.ListAPIView):
+    queryset = Policy.objects.all()
+    serializer_class = PolicySerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["category", "policy_type"]
+    search_fields = ["title"]
+    
+class PolicyCreateAPIView(generics.CreateAPIView):
+    queryset = Policy.objects.all()
+    serializer_class = PolicySerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+class PolicyUpdateAPIView(generics.UpdateAPIView):
+    queryset = Policy.objects.all()
+    serializer_class = PolicySerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+class PolicyDeleteAPIView(generics.DestroyAPIView):
+    queryset = Policy.objects.all()
+    serializer_class = PolicySerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
